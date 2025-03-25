783725cdc9446ca63bd5dc75399dc11112dcb69d
animeneek <imemyanime@gmail.com>
2025-03-25 23:33:43 +0400

    Update Info.js

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,1 +1,17 @@
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
+const Info = () => {
+  const [searchParams] = useSearchParams();
+  const malId = searchParams.get("id");
+
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-center text-2xl mt-5">Anime Info (ID: {malId})</h1>
+    </div>
+  );
+};
+
+export default Info;

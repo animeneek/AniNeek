05c2569e807bbd49bad77cbb4957888ebd346776
animeneek <imemyanime@gmail.com>
2025-03-25 23:31:32 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Info from "./pages/Info";
+import Watch from "./pages/Watch";
 
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/watch" element={<Watch />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

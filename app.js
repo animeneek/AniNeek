e1208c8af8a85655fad24a5c1c07611ae6d458bf
animeneek <imemyanime@gmail.com>
2025-03-26 11:14:59 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("AniNeek is live!");
 
     const themeToggle = document.getElementById("themeToggle");
+
     themeToggle.addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-        themeToggle.textContent = document.body.classList.contains("light-mode") ? "☀️" : "🌙";
     });
 });

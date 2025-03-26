91fcc8becac9bf904bea3dfae89a64c3e6c9f50b
animeneek <imemyanime@gmail.com>
2025-03-26 15:16:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -18,20 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
     function searchAnime() {
         const query = searchBox.value.trim();
         if (!query) return;
-
-        fetch(`https://api.jikan.moe/v4/anime?q=${query}`)
-            .then(response => response.json())
-            .then(data => {
-                animeResults.innerHTML = "";
-                data.data.forEach(anime => {
-                    const animeItem = document.createElement("div");
-                    animeItem.innerHTML = `
-                        <h3>${anime.title}</h3>
-                        <a href="info.html?id=${anime.mal_id}"><img src="${anime.images.jpg.image_url}" width="150"></a>
-                    `;
-                    animeResults.appendChild(animeItem);
-                });
-            });
+        window.location.href = `search.html?q=${query}`;
     }
 
     // Random Anime Function

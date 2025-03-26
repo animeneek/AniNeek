997fe8db579dd9d66ecd5fa8d0f90516da257380
animeneek <imemyanime@gmail.com>
2025-03-26 16:00:21 +0400

    Update theme.js

--- theme.js
+++ theme.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     function applyTheme() {
         if (localStorage.getItem("theme") === "light") {
             document.body.classList.add("light-mode");
-            toggleButton.innerHTML = '<i class="fa-solid fa-sun"></i>';
+            toggleButton.innerHTML = '<i class="fa-solid fa-toggle-on" style="color: #000000;"></i>';
         } else {
             document.body.classList.remove("light-mode");
-            toggleButton.innerHTML = '<i class="fa-solid fa-moon"></i>';
+            toggleButton.innerHTML = '<i class="fa-solid fa-toggle-off" style="color: #000000;"></i>';
         }
     }
 
